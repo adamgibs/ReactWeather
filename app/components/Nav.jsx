69acee5e331b06e3a07493643e52cc81c99bf4d8
Adam Gibson <adamgibs@outlook.com>
2016-12-04 09:07:51 -0600

    Add nav serach support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,7 +5,12 @@ var Nav = React.createClass({
 
 onSearch: function(e){
   e.preventDefault();
-  alert('Not yet wired up');
+  var location = this.refs.search.value;
+  var encodedLocation = encodeURIComponent(location);
+  if(location.length > 0){
+    this.refs.search.value = '';
+    window.location.hash = '#/?location=' + encodedLocation;
+  };
 },
   render: function() {
     return(
@@ -22,7 +27,7 @@ onSearch: function(e){
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type='serach' placeholder='Search weather by city'/>
+                <input type='serach' placeholder='Search weather by city' ref='search'/>
               </li>
               <li>
                 <input type='submit' className='button' value='Get Weather'/>
